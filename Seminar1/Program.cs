4e8151ae9cf4354f3a07e8e22359b59412c1fa3a
slavik191079 <gmail.com>
2022-06-07 14:20:49 +0300

    Наброски и первая задача из дз

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*int num; 
+
+Console.Write("Введите целое число : ");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Resultat is " + num*num);
+
+
+int num1, num2, quad;
+
+Console.Write("Input first number : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+num2= Convert.ToInt32(Console.ReadLine());
+
+quad = num2*num2;
+
+if (num1 == quad)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+
+}
+/*int num;
+
+Console.Write("Введите число ");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num*num);
+
+*/
+
+int num1 ,num2;
+
+Console.WriteLine("Введите первое число " );
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число " );
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1<num2)
+{
+    Console.WriteLine("Второе число больше и это  : " + num2 );
+}
+
+else
+{
+    Console.WriteLine("Первое число больше и это  : " + num1 );
+}
+
+
+
+
+
+
