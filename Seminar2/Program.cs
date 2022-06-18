f4cbf19679cf57cc6c86550512b6ca1887e62aca
slavik191079 <gmail.com>
2022-06-18 16:13:00 +0300

    Добавил все семинары

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -358,7 +358,7 @@ for (int i=0; i<size; i++)
 }
 ______________________________________________________________________________
 
-*/
+
 int Max(int num1,int num2,int num3)
 {
     int max=num1;
@@ -371,7 +371,37 @@ int [] pump={new Random().Next(0,2)};
 
 Console.WriteLine(result);
 
- 
+ ___________________________________________________________
+
+ Задача 1 по Семинару 4
+
+
+double TwoN ( int num1, int num2)
+{
+    return  Math.Pow ((num1),num2);   
+}
+
+Console.WriteLine("Введите число А");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень в которую хотите возвести число А");
+int b=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(TwoN(a,b));
+
+___________________________________________________________________________
+
+*/
+//Задача 2 по семинару 4
+//Можно было ввести переменную для размера элементов массива,но я не стал
+int [] massive = new int [8];
+for (int i = 0;i < 8;i++)
+{
+    massive[i]=new Random().Next(1,60);
+    Console.Write(massive[i]+" ");
+}
+
+
 
 
  
--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,79 +1,27 @@
-﻿/*
+﻿/*Задача 1 по Семинару 4
 
-Задача 1(про палиндром)
-
-void PaliN(int num)
+double TwoN ( int num1, int num2)
 {
- int  Aa = num / 10000;
- int Bb = num % 10000 / 1000;
- int Cc = num % 10;
- int Dd = num % 100 / 10;
- 
- if (Aa == Cc && Bb == Dd)
- {
-     Console.WriteLine("Уау,это палиндром!!");
- }
-
- else 
-  {
-      Console.WriteLine("Это обычное число");
-  }
-  
-}
-int num;
-Console.WriteLine("введите число");
- num = Convert.ToInt32(Console.ReadLine());
- PaliN(num);
-
-_______________________________________________
-Задача 2(Координаты в 3д)
-
-double FindLenght3D( double xA, double xB,double yA,double yB, double zA,double zB )
-{
-   return Math.Sqrt((Math.Pow((xB-xA),2)+Math.Pow((yB-yA),2)+Math.Pow((zB-zA),2)));
-      
-}
-Console.WriteLine("Введите координаты точки xA ");
-double xA=Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите координаты точки xB ");
-double xB=Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите координаты точки yA ");
-double yA=Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите координаты точки yB ");
-double yB=Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите координаты точки zA ");
-double zA=Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите координаты точки zB ");
-double zB=Convert.ToDouble(Console.ReadLine());
-
-
- double res=FindLenght3D(xA,xB,yA,yB,zA,zB);
- Console.WriteLine(res);
-
-
-_____________________________________________
-
-Задача 3(про куб)
-
-void Cub(int num)
-{
-    int i = 1;
-    while (i <= num)
-    {
-        Console.WriteLine(Math.Pow((i),3));
-        i ++;
-        
-    }
+    return  Math.Pow ((num1),num2);   
 }
 
-Console.WriteLine("введите число");
-int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число А");
+int a=Convert.ToInt32(Console.ReadLine());
 
-Cub(num1);
+Console.WriteLine("Введите степень в которую хотите возвести число А");
+int b=Convert.ToInt32(Console.ReadLine());
 
-Все...*/
+Console.WriteLine(TwoN(a,b));
+
+___________________________________________________________________________
+
+//Задача 2 по семинару 4
+//Можно было ввести переменную для размера элементов массива,но я не стал
+
+int [] massive = new int [8];
+for (int i = 0;i < 8;i++)
+{
+    massive[i]=new Random().Next(1,60);
+    Console.Write(massive[i]+" ");
+}
+*/
