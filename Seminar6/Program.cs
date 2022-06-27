8f334e26c1d6e25f0b9a61eb830d64439c6a580c
Slavik191079 <slavik191079@gmail.com>
2022-06-27 20:40:35 +0300

    seminar6

---
+++ Seminar6/Program.cs
@@ -0,0 +1,54 @@
+﻿
+/* Задача 1
+
+Console.Write("Введите колличество чисел,которое вы хотите ввести : ");
+int usernum=Convert.ToInt32(Console.ReadLine());
+
+PositivNumbers(usernum);
+
+void PositivNumbers(int num)
+{
+    int count=0;
+    int ammount=0;
+    while (ammount<usernum) 
+    {
+        Console.Write("Введите число :  " );
+        int number=Convert.ToInt32(Console.ReadLine());
+        if (number >0) count++; ammount++;
+    }
+    Console.WriteLine("Колличество положительных чисел = " + count );
+
+}
+_____________________________________
+Задача 2
+
+int [] RandomMassive(int size)
+{
+    int[] massive = new int [size];
+    for( int i=0; i<massive.Length; i++)
+    {
+        massive[i]=new Random().Next(-100,100);
+        Console.Write(massive[i] + "  ");
+    }
+    return massive;
+}
+Console.Write("Original : " + "  ");
+int [] pip=RandomMassive(8);
+
+int [] CloneMassive(int [] array)
+{
+    int[] clonearray=new int[pip.Length];
+    for(int i=0;i<clonearray.Length;i++)
+    {
+        clonearray[i]=pip[i];
+        Console.Write(clonearray[i] + "  ");
+    }
+    return clonearray;
+}
+
+Console.WriteLine();
+Console.Write("Copy     : " + "  ");
+CloneMassive(pip);
+
+*/
+
