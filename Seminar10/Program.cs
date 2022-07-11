bfcdb39c372d0ec8257ac934ae52c492dc17d0d7
Slavik191079 <slavik191079@gmail.com>
2022-07-11 21:55:35 +0300

    Seminar10

---
+++ Seminar10/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 1
+
+string [] words= { "vwe", "aer", "art", "rty", "tyu"};
+int NumWord(string [] massive)
+{
+    int count=0;
+    string letters="aeiouy";
+    for(int i=0;i<massive.Length;i++)
+    {
+        for( int j=0;j<letters.Length;j++)
+        {
+           if (massive[i][0]==letters[j]) count++; 
+        }
+        
+    }
+    return count;
+}
+Console.WriteLine(NumWord(words));
+
+
+Задача 2
+
+string [] words= { "vwe", "aer", "art", "rty", "tyu", "bub","pip"};
+string [] NumWord(string [] massive)
+{
+    string []array=new string[massive.Length];
+    for(int i=0; i<massive.Length-1;i=i+2)
+    {
+        array[i]=massive[i]+massive[i+1];
+        Console.WriteLine(array[i]);
+        
+    }   
+    return array;
+} 
+NumWord(words); */
+
